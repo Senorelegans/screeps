c67762a1055fa690f8e195c8adf69d65de3d21f0
marko melnick <senorelegans@gmail.com>
2018-06-23 11:28:41 -0600

    screep screpp mutha screepa

---
+++ marko_screeps/screeps.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+change the roles of screeps
+Game.creeps['Harvester1'].memory.role = 'harvester';
+Game.creeps['Harvester2'].memory.role = 'builder';
+Game.creeps['Harvester2'].memory.role = 'upgrader';
+
+Game.creeps['poop'].memory.role = 'upgrader';
+
+
+
+spawn harvester
+Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], "Harvester1",
+    {memory: {role: 'harvester',working:false}});
+
+
+move screeps
+Game.creeps['Harvester1'].move(RIGHT);
+
+
+console log
+console.log(creep.memory.role);
